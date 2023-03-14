8f1bfe99bbfb2ed7eb6eff4bc5d6343c1b82e2a7
Love Dittlau <love@dittlau.se>
2023-03-14 11:50:31 +0100

    initial commit

---
+++ includes/nav.php
@@ -0,0 +1,5 @@
+<nav class="navbar">
+    <button><i class="fa fa-home" aria-hidden="true"></i></button>
+    <button><i class="fa fa-play" aria-hidden="true"></i></button>
+    <button><i class="fa fa-user" aria-hidden="true"></i></button>
+</nav>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+
+<?php include "includes/header.php"; ?>
+
+<body>
+<main>
+    <p>main</p>
+</main>
+
+<?php include "includes/nav.php"; ?>
+</body>
+</html>
