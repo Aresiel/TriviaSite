52af45b3d47ef0a1b34c6d6c93cd80bd2150f566
Love Dittlau <love@dittlau.se>
2023-03-16 10:59:34 +0100

    title with animation

---
+++ home.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+
+<?php include "includes/header.php"; ?>
+
+<body>
+<main>
+    <div class="big-title">
+        <span>Trivia</span>
+        <span>Game</span>
+    </div>
+</main>
+
+<?php include "includes/nav.php"; ?>
+</body>
+</html>
--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,13 @@
+<?php
+    $selectionMap = array(
+            "/home.php" => "home",
+            "/play.php" => "game"
+    );
+    $currentFile = $selectionMap[$_SERVER['PHP_SELF']];
+?>
+
 <nav class="navbar">
-    <button><i class="fa fa-home" aria-hidden="true"></i></button>
-    <button><i class="fa fa-play" aria-hidden="true"></i></button>
-    <button><i class="fa fa-user" aria-hidden="true"></i></button>
+    <a <?php if ($currentFile != "home") echo "href=\"/home.php\""; ?> class="<?php if ($currentFile == "home") echo "selected"; ?>"><i class="fa fa-home" aria-hidden="true"></i></a>
+    <a <?php if ($currentFile != "game") echo "href=\"/play.php\""; ?> class="<?php if ($currentFile == "game") echo "selected"; ?>"><i class="fa fa-play" aria-hidden="true"></i></a>
+    <a class="<?php if ($currentFile == "user") echo "selected"; ?>"><i class="fa fa-user" aria-hidden="true"></i></a>
 </nav>
