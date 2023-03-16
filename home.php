73cb50e7ae68d077f1ed86f0e55c226df134b009
Love Dittlau <love@dittlau.se>
2023-03-16 11:47:35 +0100

    adjustments

--- home.php
+++ home.php
@@ -9,6 +9,10 @@
         <span>Trivia</span>
         <span>Game</span>
     </div>
+
+    <div class="big-play">
+        <a href="/play.php" class="fa fa-play" aria-hidden="true"></a>
+    </div>
 </main>
 
 <?php include "includes/nav.php"; ?>
