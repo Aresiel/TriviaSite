464cd5412b43d678d67a46e7d27716736f18fe8b
Love Dittlau <love@dittlau.se>
2023-03-23 14:10:05 +0100

    add support to active

--- includes/nav.php
+++ includes/nav.php
@@ -3,11 +3,11 @@
             "/home.php" => "home",
             "/play.php" => "game"
     );
-    $currentFile = $selectionMap[$_SERVER['PHP_SELF']];
+    $currentSection = $selectionMap[$_SERVER['PHP_SELF']];
 ?>
 
 <nav class="navbar">
-    <a <?php if ($currentFile != "home") echo "href=\"/home.php\""; ?> class="<?php if ($currentFile == "home") echo "selected"; ?>"><i class="fa fa-home" aria-hidden="true"></i></a>
-    <a <?php if ($currentFile != "game") echo "href=\"/play.php\""; ?> class="<?php if ($currentFile == "game") echo "selected"; ?>"><i class="fa fa-play" aria-hidden="true"></i></a>
-    <a class="<?php if ($currentFile == "user") echo "selected"; ?>"><i class="fa fa-user" aria-hidden="true"></i></a>
+    <a <?php if ($currentSection != "home") echo "href=\"/home.php\""; ?> class="<?php if ($currentSection == "home") echo "selected"; ?>"><i class="fa fa-home" aria-hidden="true"></i></a>
+    <a <?php if ($currentSection != "game") echo "href=\"/play.php\""; ?> class="<?php if ($currentSection == "game") echo "selected"; ?>"><i class="fa fa-play" aria-hidden="true"></i></a>
+    <a class="<?php if ($currentSection == "user") echo "selected"; ?>"><i class="fa fa-user" aria-hidden="true"></i></a>
 </nav>
